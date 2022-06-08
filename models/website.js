f9165511835f6cc0a0865e253fab4ff8dcc1b38e
jhuang15 <jchuang1020@gmail.com>
2022-06-08 18:58:27 -0400

    Add New Invite Feature Created

--- models/website.js
+++ models/website.js
@@ -1,19 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const inviteSchema = new Schema(
-  {
-    content: {
-      type: String
-    },
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    userName: String,
-  },
-  {
-    timestamps: true,
-  }
-);
-
 const websiteSchema = new Schema(
   {
     home: {
--- src/components/HomeWebsiteForm/HomeWebsiteForm.jsx
+++ src/components/HomeWebsiteForm/HomeWebsiteForm.jsx
@@ -14,15 +14,21 @@ export default class HomeWebsiteForm extends Component {
           photos: '',
           invite: ''
         }]
-        
       };
+      this.handleSubmit = this.handleSubmit.bind(this)
     }
+
     handleSubmit = async (evt) => {
       evt.preventDefault();
-      alert('A name was submitted: ' + this.state.name1);
+      alert(JSON.stringify(this.state.formValues));
       
     }
-    handleChange = (evt) => {
+    handleChange = (i, evt) => {
+
+      let formValues = this.state.formValues;
+      formValues[i][evt.target.name] = evt.target.value;
+      this.setState({ formValues });
+      /*
       this.setState({
         [evt.target.name1]: evt.target.value, 
         [evt.target.name2]: evt.target.value,
@@ -30,6 +36,19 @@ export default class HomeWebsiteForm extends Component {
         [evt.target.location]: evt.target.value,
         [evt.target.story]: evt.target.value
       });
+      */
+    }
+
+    addFormFields() {
+      this.setState(({
+        formValues: [...this.state.formValues, { invite: ""}]
+      }))
+    }
+  
+    removeFormFields(i) {
+      let formValues = this.state.formValues;
+      formValues.splice(i, 1);
+      this.setState({ formValues });
     }
 
   render() {
@@ -55,12 +74,24 @@ export default class HomeWebsiteForm extends Component {
           <input type="file" value={this.state.photos} onChange={this.handleChange} />
         </div>
 
-        <div className="website-rsvp">
+        <div className="website-rsvp" >
           <h3>RSVP</h3>
           <p>This will show the user the guest list: accept/decline. Will nto be displayed to guests</p>
-          <input type="text" value={this.state.invite} onChange={this.handleChange} placeholder="Name of guest" />
+
+        {this.state.formValues.map((element, index) => (
+          <div className="add-invite-input" key={index}>
+           <input type="text" value={element.invite || ""} onChange={e => this.handleChange(index, e)} placeholder="Name of guest" />
+           { index ? 
+             <button type="button" className="btn-remove" onClick={() => this.removeFormFields(index)}>Remove</button>
+             : null
+           }
+          </div>
+        ))}
+        <div className="button-section">
+          <button className="btn-add" type="button" onClick={() => this.addFormFields()}>Add</button>
+          <button className="btn-submit" type="submit">Submit</button>
         </div>
-        
+        </div>
       </form>
       </div>
     );
